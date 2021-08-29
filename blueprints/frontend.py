c2755fda90b7895ab688a2f4febb74711006705e
Sensokaku <28125632+Sensokaku@users.noreply.github.com>
2021-08-29 13:11:27 +0800

    miksu things
    miksu theme + added stuff

--- blueprints/frontend.py
+++ blueprints/frontend.py
@@ -569,6 +569,10 @@ async def logout():
 
     # render login
     return await flash('success', 'Successfully logged out!', 'login')
+    
+@frontend.route('/rules')
+async def rules():
+    return await render_template('rules.html')
 
 # social media redirections
 
